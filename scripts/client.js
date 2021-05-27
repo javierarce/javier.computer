310a0c7be221d49eff058d9c551fe36d7cd1bdf7
Javier Arce <4933+javierarce@users.noreply.github.com>
2021-05-27 14:23:06 +0200

    Autoreload

--- scripts/client.js
+++ scripts/client.js
@@ -1,16 +1,17 @@
+let currentSong = {}
+
 const loadLastSong = () => {
   const headers = { 'Content-Type': 'application/json' }
   const method = 'GET'
   const options = { method, headers }
   const URL = '//last.javierarce.com/api/song'
 
-  fetch(URL).then(response => response.json())
-    .then((json) => {
-      if (json.error) {
-        return console.error(json)
-      }
-      addSong(json)
-    })
+  fetch(URL).then(response => response.json()).then((json) => {
+    if (json.error) {
+      return console.error(json)
+    }
+    addSong(json)
+  })
 }
 
 const addSong = (song) => {
@@ -18,21 +19,31 @@ const addSong = (song) => {
     return
   }
 
-  let $song = document.createElement('div')
-  $song.classList.add('Song')
+  let $song = document.body.querySelector('.js-song')
+
+  if (!$song) {
+    $song = document.createElement('div')
+    $song.classList.add('Song')
+    $song.classList.add('js-song')
+    let $intro = document.querySelector('.js-intro')
+    $intro.appendChild($song)
+  } else if (currentSong && currentSong.name && currentSong.name !== song.name) {
+    $song.classList.remove('is-visible')
+  }
+
   $song.innerHTML = `I'm listening to <strong>${song.name}</strong> by ${song.artist}.`
-  let $intro = document.querySelector('.js-intro')
-  $intro.appendChild($song)
 
   setTimeout(() => {
     $song.classList.add('is-visible')
-  })
+  }, 500)
+
+  currentSong = song
 }
 
 const onLoad = () => {
   new Chameleon(['twitter', 'gmail', 'tumblr'])
   loadLastSong()
+  setInterval(loadLastSong, 30 * 1000)
 }
-
 
 window.onload = onLoad
