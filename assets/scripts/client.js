c3cd5c3fdae7679d9795593d88e886e5ca85391f
Javier Arce <4933+javierarce@users.noreply.github.com>
2021-12-21 22:55:15 +0100

    test

--- assets/scripts/client.js
+++ assets/scripts/client.js
@@ -1,11 +1,12 @@
 const onLoad = () => {
   new Chameleon(['twitter', 'gmail', 'wikipedia', 'tumblr'])
-  new Song('.js-nav')
-  new Weather()
 
   window.lazyLoadInstance = new LazyLoad({
     elements_selector: '.lazy'
   })
+
+  new Song('.js-nav')
+  new Weather()
 }
 
 window.onload = onLoad
