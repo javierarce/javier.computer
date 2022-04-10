65ee6f5313e62cb19c11f55a5c6a3a3a3f05e300
Javier Arce <4933+javierarce@users.noreply.github.com>
2022-04-10 22:36:11 +0200

    Link

--- docs/assets/scripts/Song.js
+++ docs/assets/scripts/Song.js
@@ -1,51 +1,76 @@
-function onYouTubeIframeAPIReady() {
-  let audio = document.getElementById("youtube-audio")
-  let  $el = document.createElement("div")
-  $el.innerHTML = 'play'
+const SONG_URL = '//last.javierarce.com/api/song'
+const TEMPLATE = "I'm listening to <strong>NAME</strong> by ARTIST"
+const INTERVAL = 30 * 1000
 
-  $el.setAttribute("id", "youtube-icon")
-  $el.style.cssText = "cursor:pointer;cursor:hand"
-  audio.appendChild($el)
-
-  let $div = document.createElement("div")
-
-  $div.setAttribute("id", "youtube-player"), audio.appendChild($div)
-
-  let o = function (e) {
-    if (e) {
-      $el.innerHTML = 'play'
-    } else {
-      $el.innerHTML = 'stop'
+class Song {
+  constructor (parentClassName) {
+    this.currentSong = {}
+    this.$parent = document.body.querySelector(parentClassName)
+    if (this.$parent) {
+      this.get()
     }
-  };
 
-  audio.onclick = function() {
-    if (player.getPlayerState() === YT.PlayerState.PLAYING || player.getPlayerState() === YT.PlayerState.BUFFERING) {
-      player.pauseVideo()
-      o(0)
-    } else { 
-      player.playVideo()
-      o(1)
-    }
+    setInterval(() => { this.get() }, INTERVAL)
   }
 
-  let dataset = audio.dataset
+  show () {
+    this.$element.classList.add('is-visible')
+  }
 
-  let player = new YT.Player("youtube-player", {
-    height: "0",
-    width: "0",
-    videoId: dataset.video,
-    playerlets: {
-      autoplay: dataset.autoplay,
-      loop: dataset.loop
-    },
-    events: {
-      onReady: function(e) {
-        player.setPlaybackQuality("small"), o(player.getPlayerState() !== YT.PlayerState.CUED)
-      },
-      onStateChange: function(e) {
-        audio.data === YT.PlayerState.ENDED && o(!1)
+  hide () {
+    this.$element.classList.remove('is-visible')
+  }
+
+  get () {
+    const headers = { 'Content-Type': 'application/json' }
+    const method = 'GET'
+    const options = { method, headers }
+
+    fetch(SONG_URL).then(response => response.json()).then((json) => {
+      if (json.error) {
+        return console.error(json)
       }
+
+      this.song = json && json.name ? json : undefined
+      this.render()
+    }).catch((error) => {
+      console.error(error)
+      //this.song = { name: 'Name', artist: 'Arist'}
+      //this.render()
+    })
+  }
+
+  createElement () {
+    let $element = document.createElement('div')
+    $element.classList.add('Song')
+    $element.classList.add('js-song')
+
+    return $element
+  }
+
+  render () {
+    this.$element = document.body.querySelector('.js-song')
+
+    if (!this.song) {
+      if (this.$element) {
+        this.hide()
+      }
+      return
     }
-  })
+
+    if (!this.$element) {
+      this.$element = this.createElement()
+      this.$parent.prepend(this.$element)
+    } else if (this.currentSong.name && this.currentSong.name !== this.song.name) {
+      this.hide()
+    }
+
+    this.$element.innerHTML = TEMPLATE.replace('NAME', this.song.name).replace('ARTIST', this.song.artist)
+
+    setTimeout(() => {
+      this.show()
+    }, 500)
+
+    this.currentSong = this.song
+  }
 }
