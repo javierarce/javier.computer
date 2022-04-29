e23cb853d78a6d98f53150f74d190a53a6e1b4de
Javier Arce <4933+javierarce@users.noreply.github.com>
2022-04-29 11:01:56 +0200

    Limited-time offer

---
+++ _plugins/photo.rb
@@ -0,0 +1,29 @@
+class Photo < Liquid::Tag
+  def initialize(tag_name, filename, tokens)
+    super
+    @filename = filename.strip
+  end
+
+  def render(context)
+    "<div class='Photo'>
+    <div class='Photo__content'>
+    <figure class='Figure'>
+        <picture>
+
+          <source data-srcset='https://img.javier.computer/#{@filename}_640.webp 640w, https://img.javier.computer/#{@filename}_1280.webp 1280w, https://img.javier.computer/#{@filename}_2880.webp 2880w' type='image/webp'>
+
+          <source data-srcset='https://img.javier.computer/#{@filename}_640.jpg 640w, https://img.javier.computer/#{@filename}_1280.jpg 1280w, https://img.javier.computer/#{@filename}_2880.jpg 2880w'>
+
+          <img class='Photo lazy' data-src='https://img.javier.computer/#{@filename}_1280.jpg'>
+
+          </picture>
+      </figure>
+         </div>  
+
+      </div>  
+
+    "
+  end
+end
+
+Liquid::Template.register_tag('photo', Photo)
---
+++ content/_posts/2022-04-29-limited-time-offer.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title: "Limited-time offer"
+date: "2022-04-29 10:55:15 +0200"
+---
+
+I'm giving away some of my Yee-haw! stickers to friends and readers
+of this blog. Find [how to get one here](/sticker).
