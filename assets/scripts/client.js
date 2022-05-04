e2505e7b9aa007b76a64495ca24a065733c4016e
Javier Arce <4933+javierarce@users.noreply.github.com>
2022-05-04 12:20:58 +0200

    Test

--- assets/scripts/client.js
+++ assets/scripts/client.js
@@ -1,7 +1,11 @@
 const submitPoll = (event) => {
   let name = event.target.elements.Name.value
   const URL = 'https://api.javier.computer/api/poll/poll/save'
-  const headers = { 'Content-Type': 'application/json' }
+  const headers = {
+    "Access-Control-Allow-Headers": "*", // this will allow all CORS requests
+    "Access-Control-Allow-Methods": 'OPTIONS,POST,GET', // this states the allowed methods
+    "Content-Type": "application/json" // this shows the expected content type
+  }
   const method = 'POST'
   const body = JSON.stringify({ name: name })
   const options = { method, headers, body }
