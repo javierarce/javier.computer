14b8922b988be8d7b9935cbd47a95d7132c24507
Javier Arce <4933+javierarce@users.noreply.github.com>
2022-05-04 14:17:03 +0200

    Test

---
+++ assets/scripts/Poll.js
@@ -0,0 +1,50 @@
+const submitPoll = (event) => {
+  let fields = {}
+
+  let spinner = new Spinner('is-inside-button')
+  event.target.appendChild(spinner.$element)
+  spinner.show()
+
+  function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+  }
+
+  const error = (error) => {
+    event.target.classList.add('is-error')
+    event.target.classList.remove('is-loading')
+
+    spinner.hide()
+    spinner.$element.remove()
+  }
+
+  event.target.classList.add('is-loading')
+
+  let values = [... event.target.elements].forEach((field) => {
+    if (field.type === 'text' && field.value) {
+      fields[field.name] = field.value
+    }
+  })
+
+  if (isEmpty(fields)) {
+    error(0)
+    return
+  }
+
+  const URL = 'https://api.javier.computer/api/poll/poll/save'
+  const headers = { 'Content-Type': 'application/json' }
+  const method = 'POST'
+  const body = JSON.stringify(fields)
+  const options = { method, headers, body }
+
+  fetch(URL, options).then((response) => {
+    event.target.classList.remove('is-loading')
+    spinner.hide()
+
+    if (response.status === 200) {
+      event.target.classList.add('is-success')
+    }
+  }).catch((e) => {
+    error(e)
+  })
+}
+
--- test.md
+++ test.md
@@ -1,16 +1,19 @@
 ---
-title: test
+title: Projects
 permalink: test
-layout: base
+layout: table
+className: Table
 ---
+
+¿Qué estás leyendo?
 
 <form class="Poll" method="post" onsubmit="submitPoll(event); return false">
 <div class="Poll__success">Thanks!</div>
 <div class="Poll__fields">
-  <div class="Input__field">
-  <input type="text" class="Input" name="Name" value="">
-  </div>
-  <input type="submit" name="" class="Button Poll__submit js-button" value="Send">
+<div class="Input__field">
+<input type="text" class="Input" name="Name" value="" placeholder="Título">
+</div>
+<input type="submit" name="" class="Button Poll__submit js-button" value="Enviar">
 </div>
 </form>
 
