c859c9f8afab4617ee105c83baa490af3e334315
Javier Arce <4933+javierarce@users.noreply.github.com>
2022-05-04 13:04:07 +0200

    test

--- assets/scripts/client.js
+++ assets/scripts/client.js
@@ -1,11 +1,38 @@
 const submitPoll = (event) => {
-  let name = event.target.elements.Name.value
+  let fields = {}
+  function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+  }
+  
+  const error = (error) => {
+    event.target.classList.add('is-error')
+  }
+
+  let values = [... event.target.elements].forEach((field) => {
+    if (field.type === 'text' && field.value) {
+      fields[field.name] = field.value
+    }
+  })
+
+  if (isEmpty(fields)) {
+    error(0)
+    return
+  }
+
   const URL = 'https://api.javier.computer/api/poll/poll/save'
+  //const URL = 'http://localhost:3001/api/poll/poll/save'
   const headers = { 'Content-Type': 'application/json' }
   const method = 'POST'
-  const body = JSON.stringify({ name: name })
+  const body = JSON.stringify(fields)
   const options = { method, headers, body }
-  fetch(URL, options)
+
+  fetch(URL, options).then((response) => {
+    if (response.status === 200) {
+      event.target.classList.add('is-success')
+    }
+  }).catch((e) => {
+    error(e)
+  })
 }
 
 const onLoad = () => {
