af1ed890a419db1d8bd0aa910e0051207bff984a
Javier Arce <4933+javierarce@users.noreply.github.com>
2022-09-24 19:05:30 +0200

    Reverse

--- assets/scripts/bots.js
+++ assets/scripts/bots.js
@@ -1,18 +1,22 @@
 window.onload = () => {
   const filename = window.location.pathname.split('/').pop()
   if (filename) {
-    fetch(`https://bots.javier.computer/${filename}.json`).then(response => response.json()).then((data) => {
+    fetch(`https://bots.javier.computer/${filename}.json?v=${Math.random() * 1000}`).then(response => response.json()).then((data) => {
       console.log(data)
       
       let $content = document.querySelector('.js-content')
 
-      data.items.forEach((text) => {
+      let $items = document.createElement('div')
+      $items.classList.add('Bot__items')
+
+      data.items.reverse().forEach((text) => {
         let $item = document.createElement('div')
         $item.classList.add('Bot__item')
         $item.innerHTML = text
-        $content.appendChild($item)
+        $items.appendChild($item)
       })
 
+      $content.appendChild($items)
       $content.classList.add('is-visible')
     })
   }
