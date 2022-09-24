272507c16ef0ad0c8b9663aad5ed80fc8f53b1ef
Javier Arce <4933+javierarce@users.noreply.github.com>
2022-09-24 17:10:50 +0200

    Fix json fetch

--- assets/scripts/bots.js
+++ assets/scripts/bots.js
@@ -2,10 +2,11 @@ window.onload = () => {
   const filename = window.location.pathname.split('/').pop()
   if (filename) {
     fetch(`https://bots.javier.computer/${filename}.json`).then(response => response.json()).then((data) => {
+      console.log(data)
       
       let $content = document.querySelector('.js-content')
 
-      data.forEach((text) => {
+      data.items.forEach((text) => {
         let $item = document.createElement('div')
         $item.classList.add('Bot__item')
         $item.innerHTML = text
