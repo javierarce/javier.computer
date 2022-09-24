870b3e4cdb3e36e87df9686c7135afe6ca6d875c
Javier Arce <4933+javierarce@users.noreply.github.com>
2022-09-24 15:43:54 +0200

    Transition

--- assets/scripts/bots.js
+++ assets/scripts/bots.js
@@ -4,12 +4,15 @@ window.onload = () => {
     fetch(`https://bots.javier.computer/${filename}.json`).then(response => response.json()).then((data) => {
       
       let $content = document.querySelector('.js-content')
+
       data.forEach((text) => {
         let $item = document.createElement('div')
         $item.classList.add('Bot__item')
         $item.innerHTML = text
         $content.appendChild($item)
       })
+
+      $content.classList.add('is-visible')
     })
   }
 }
