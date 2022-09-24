8cd077727179a3780617d6bc36371f967a8fc92f
Javier Arce <4933+javierarce@users.noreply.github.com>
2022-09-24 22:50:58 +0200

    Fixes css

--- assets/scripts/bots.js
+++ assets/scripts/bots.js
@@ -5,12 +5,11 @@ window.onload = () => {
       console.log(data)
       
       let $content = document.querySelector('.js-status')
+      let $archive = document.querySelector('.js-archive')
       let $updatedAt = document.querySelector('.js-updated-at')
 
-      let today = new Date(data.updatedAt)
-  const prefix = `${today.getFullYear()}-${(today.getMonth() + 1)
-  .toString()
-  .padStart(2, '0')}-${today.getDate().toString().padStart(2, '0')} at ${today.getHours()}:${today.getMinutes()}`
+      const today = new Date(data.updatedAt)
+      const prefix = `${today.getFullYear()}-${(today.getMonth() + 1).toString().padStart(2, '0')}-${today.getDate().toString().padStart(2, '0')} at ${today.getHours()}:${today.getMinutes()}`
 
 
       let $items = document.createElement('div')
@@ -24,7 +23,7 @@ window.onload = () => {
       })
 
       $content.appendChild($items)
-      $content.classList.add('is-visible')
+      $archive.classList.add('is-visible')
       $updatedAt.innerHTML = prefix
 
     })
