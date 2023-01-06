0f8e1d287e8718d079411a7461901cbb7cbb7270
Javier Arce <4933+javierarce@users.noreply.github.com>
2023-01-06 13:13:35 +0100

    Music Snitch

---
+++ scripts/music.js
@@ -0,0 +1,52 @@
+class Snitch extends HTMLDivElement {
+  constructor () {
+    super()
+  }
+
+  connectedCallback () {
+    this.get()
+  }
+
+  get () {
+    console.log('test')
+    const options = {
+      username: 'javierarce',
+      api_key: '78b4ae34c84de1d5fc6510338300bd78'
+    }
+    const URL  = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${options.username }&api_key=${options.api_key }&limit=1&format=json`
+
+    fetch(URL).then(response => response.json()).then((data) => {
+      const tracks = data.recenttracks.track
+      const track = tracks[0]
+      this.render(`${track.name} by ${track.artist['#text']}`, track.url)
+    })
+  }
+
+  render (description, URL) {
+    const text = document.createElement('a')
+    text.href = URL
+    text.textContent = description
+    text.target = '_blank'
+    text.part = 'a'
+
+    const title = document.createElement('strong')
+    title.textContent = 'Listening'
+    title.part = 'title'
+
+    const close = document.createElement('button')
+    close.textContent = '×'
+    close.part = 'close'
+    close.onclick = () => {
+      this.classList.remove('is-visible')
+    }
+
+    let shadow = this.attachShadow({ mode: 'open' })
+    shadow.appendChild(title)
+    shadow.appendChild(close)
+    shadow.appendChild(text)
+
+    this.classList.add('is-visible')
+  }
+}
+
+customElements.define('music-snitch', Snitch, { extends: 'div' })
