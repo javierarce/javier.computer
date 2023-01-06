78b5aa6b124908b3e5e2e946f79e10085fe502f7
Javier Arce <4933+javierarce@users.noreply.github.com>
2023-01-06 19:56:14 +0100

    Mobile

--- scripts/music.js
+++ scripts/music.js
@@ -3,42 +3,67 @@ class Snitch extends HTMLElement {
     super()
   }
 
-  connectedCallback(e) {
+  connectedCallback() {
+    this.fetchScrobbler()
+
+    setInterval(() => {
+      this.fetchScrobbler()
+    }, 30 * 1000)
+  }
+
+  fetchScrobbler () {
     const username = this.getAttribute("data-username")
     const key = this.getAttribute("data-key")
-    const URL  = `//ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username }&api_key=${key }&limit=1&format=json`
+    const URL  = `//ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${key}&limit=1&format=json`
 
     fetch(URL).then(response => response.json()).then((data) => {
-      const tracks = data.recenttracks.track
-      const track = tracks[0]
-      this.render(`${track.name} by ${track.artist['#text']}`, track.url)
+      const track = data.recenttracks.track[0]
+
+      if (track) {
+        this.render(track.name, track.artist, track.url)
+      }
     })
   }
 
-  render (description, URL) {
-    const text = document.createElement('a')
-    text.href = URL
-    text.textContent = description
-    text.target = '_blank'
-    text.part = 'a'
+  render (name, artist, URL) {
+    if (!this.shadow) {
+      const text = document.createElement('a')
+      text.href = URL
+      text.textContent = `${name} by ${artist['#text']}`
+      text.target = '_blank'
+      text.part = 'a'
 
-    const title = document.createElement('strong')
-    title.textContent = 'Listening'
-    title.part = 'title'
+      const title = document.createElement('strong')
+      title.textContent = 'Listening'
+      title.part = 'title'
 
-    const close = document.createElement('button')
-    close.textContent = '×'
-    close.part = 'close'
-    close.onclick = () => {
-      this.classList.remove('is-visible')
+      const close = document.createElement('button')
+      close.textContent = '×'
+      close.part = 'close'
+      close.onclick = () => {
+        this.classList.remove('is-visible')
+      }
+
+      this.shadow = this.attachShadow({ mode: 'open' })
+      this.shadow.appendChild(title)
+      this.shadow.appendChild(close)
+      this.shadow.appendChild(text)
+
+      this.classList.add('is-visible')
+    } else {
+      const text = this.shadow.querySelector('a')
+
+      const oldURL = text.href
+      text.textContent = `${name} by ${artist['#text']}`
+      text.href = URL
+
+      if (URL !== oldURL) {
+        this.classList.remove('is-visible')
+        setTimeout(() => {
+          this.classList.add('is-visible')
+        }, 500)
+      }
     }
-
-    let shadow = this.attachShadow({ mode: 'open' })
-    shadow.appendChild(title)
-    shadow.appendChild(close)
-    shadow.appendChild(text)
-
-    this.classList.add('is-visible')
   }
 }
 
