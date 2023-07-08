be4e42a59ed13517b242d347f6132bf52c092d35
Javier Arce <4933+javierarce@users.noreply.github.com>
2023-07-08 13:17:59 +0200

    test

--- _plugins/places.rb
+++ _plugins/places.rb
@@ -1,18 +1,5 @@
 require 'yaml'
 
-class CustomClassLoader < Psych::ClassLoader
-  def find(name)
-    if name == 'Date'
-      Date
-    else
-      super
-    end
-  end
-end
-
-loader = Psych::Visitors::ToRuby.new(CustomClassLoader.new)
-Psych::Visitors::Visitor.new(loader).accept(Psych.parse(yaml_data))
-
 module Jekyll
   class LocationFileGenerator < Generator
     safe true
@@ -25,7 +12,7 @@ module Jekyll
       # Load base location data
       Dir.glob('_data/locations/*_base.yaml').each do |base_file|
         location_name = File.basename(base_file, '_base.yaml').sub('_base', '')
-        base_locations = YAML.load_file(base_file) || []
+        base_locations = YAML.load_file(base_file, permitted_classes: [Matrix, OpenStruct, Symbol]) || []
         
         if base_locations.is_a? Array
           base_locations.each do |base_location|
@@ -61,7 +48,7 @@ module Jekyll
         yaml_file_path = "_data/locations/#{location}.yaml"
 
         if File.exists?(yaml_file_path)
-          current_data = YAML.load_file(yaml_file_path)
+          current_data = YAML.load_file(yaml_file_path, permitted_classes: [Matrix, OpenStruct, Symbol])
           next if current_data == data
         end
 
