4e01d69c6ea490814940288b2c765f7e789cbb42
Javier Arce <4933+javierarce@users.noreply.github.com>
2023-08-14 08:35:43 +0200

    Removes _site from github

--- _site/copy_feeds.rb
+++
@@ -1,7 +0,0 @@
-require 'fileutils'
-
-source_dir = File.expand_path('feeds')
-output_dir = File.expand_path('docs/feeds')
-
-FileUtils.mkdir_p(output_dir)
-FileUtils.cp_r(Dir.glob(File.join(source_dir, '*')), output_dir)
