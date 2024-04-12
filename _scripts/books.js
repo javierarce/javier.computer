ec59c8b8a6693af246bcdd25e7b7d48a76caa3e9
Javier Arce <4933+javierarce@users.noreply.github.com>
2024-04-12 19:21:53 +0200

    Books

--- _scripts/books.js
+++ _scripts/books.js
@@ -1,74 +1,73 @@
-const fs = require('fs')
-const csv = require('csv-parser')
+const fs = require('fs');
+const csv = require('csv-parser');
 
-const TEMPLATE = `---
-title: "TITLE"
-layout: book
----
-CONTENT
-`
-
-const string_to_slug = (str) => {
-  str = str.replace(/^\s+|\s+$/g, ''); // trim
-  str = str.toLowerCase()
-
-  // remove accents, swap ñ for n, etc
-  let from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;"
-  let to   = "aaaaeeeeiiiioooouuuunc------"
-
-  for (let i=0, l=from.length ; i<l ; i++) {
-    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i))
+const stringToSlug = (str) => {
+  str = str.trim().toLowerCase();
+  const from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  const to   = "aaaaeeeeiiiioooouuuunc------";
+  
+  for (let i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
   }
-
+  
   str = str.replace(/[^a-z0-9 -]/g, '') 
     .replace(/\s+/g, '-') 
-    .replace(/-+/g, '-')
-
-  return str
+    .replace(/-+/g, '-');
+  
+  return str;
 }
+
+const ratingToStars = (rating) => {
+  const numRating = parseInt(rating, 10);
+  return '★'.repeat(numRating) + '☆'.repeat(5 - numRating);
+};
 
 const createFile = (title, books) => {
-
-  let content = '| Title | Added | Read |\n'
-    content += '|:-------|:-----|:-----|\n'
+  let content = `---
+title: "TITLE"
+layout: book
+author: AUTHOR
+started: STARTED_DATE
+read: READ_DATE
+status: STATUS
+rating: "RATING_STARS"
+cover: 
+pages: PAGES
+---
+`;
 
   books.forEach((book) => {
-    let stars = ''
+    const fileContent = content
+      .replace('TITLE', book['title'])
+      .replace('AUTHOR', book['author'])
+      .replace('STARTED_DATE', book['date added'] || '')
+      .replace('READ_DATE', book['date read'] || '')
+      .replace('STATUS', book['date read'] ? 'read' : 'to-read')
+      .replace('RATING_STARS', ratingToStars(book['my rating']))
+      .replace('PAGES', book['number of pages']);
 
-    for (let i = 0, len = book['my rating']; i < len; i++) {
-      stars += "☆"
-    }
+    let path = `content/_books/${stringToSlug(book['title'])}.md`;
 
-    let publisher = book['publisher'] ? book['publisher'].trim() : 'Unknown'
 
-    content += `|**${book.title}**| ${book['date added']} | ${book['date read']}|\n`
-
-  })
-
-  let fileContent = TEMPLATE.replace('TITLE', title).replace('CONTENT', content)
-  let path = `content/_books/${string_to_slug(title)}.md`
-
-  fs.writeFile(path, fileContent, err => {
-    if (err) {
-      console.error(err)
-      return
-    }
-  })
+    fs.writeFile(path, fileContent, err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(`File written: ${path}`);
+    });
+  });
 }
 
-let toRead = []
-let books = []
+let books = [];
 
 fs.createReadStream('_data/goodreads_library_export.csv')
   .pipe(csv())
   .on('data', (data) => {
-    if (data['bookshelves'] === 'to-read') {
-      toRead.push(data)
-    } else {
-      books.push(data)
+    if (data['exclusive shelf'] === 'read') { // Ensure we're checking the correct field
+      books.push(data);
     }
   })
   .on('end', () => {
-    //createFile('Antilibrary', toRead)
-    createFile('Read2', books)
-  })
+    createFile('Read Books', books); // Change title as needed
+  });
