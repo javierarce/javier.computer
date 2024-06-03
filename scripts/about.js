11f7944790ced7839cc5dfeaf9347086fca046dc
Javier Arce <4933+javierarce@users.noreply.github.com>
2024-06-03 09:46:08 +0200

    Fixes loading of some scripts

--- scripts/about.js
+++ scripts/about.js
@@ -1,9 +1,6 @@
 const onLoad = () => {
   new Chameleon(['twitter', 'gmail', 'wikipedia', 'tumblr'])
-
-  window.lazyLoadInstance = new LazyLoad({
-    elements_selector: '.lazy'
-  })
+  new Weather()
 }
 
 window.onload = onLoad
