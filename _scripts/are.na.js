b30fe952697b9f4f10d09f999c2acef22d277d4f
Javier Arce <4933+javierarce@users.noreply.github.com>
2024-07-02 10:02:13 +0200

    Formats file

--- _scripts/are.na.js
+++ _scripts/are.na.js
@@ -1,45 +1,51 @@
-const fs = require('fs')
-const Arena = require("are.na")
+const fs = require("fs");
+const Arena = require("are.na");
 
-const CHANNEL_NAME = 'concept-net'
+const CHANNEL_NAME = "concept-net";
 
 const TEMPLATE = `---
 title: TITLE
 layout: wiki_post
 ---
 CONTENT
-`
+`;
 
 const createFile = (title, content) => {
-  let path = `content/_concepts/${title}.md`
+  let path = `content/_concepts/${title}.md`;
 
   if (fs.existsSync(path)) {
-    let currentContent = fs.readFileSync(path, 'utf-8')
+    let currentContent = fs.readFileSync(path, "utf-8");
 
     if (title && currentContent !== content) {
-      path = path.replace(`${title}.md`, `${title}_${new Date().toISOString()}.md`)
+      path = path.replace(
+        `${title}.md`,
+        `${title}_${new Date().toISOString()}.md`,
+      );
     }
   }
 
-  fs.writeFile(path, content, err => {
+  fs.writeFile(path, content, (err) => {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      return;
     }
-  })
-}
+  });
+};
 
 const onChannel = (channel) => {
   channel.map((item) => {
-    createFile(item.title, TEMPLATE.replace('TITLE', item.title).replace('CONTENT', item.content))
-  })
-}
+    createFile(
+      item.title,
+      TEMPLATE.replace("TITLE", item.title).replace("CONTENT", item.content),
+    );
+  });
+};
 
-const arena = new Arena()
+const arena = new Arena();
 arena
   .channel(CHANNEL_NAME)
   .contents({ page: 1, per: 300 })
   .then(onChannel)
   .catch((error) => {
-    console.error(error)
-  })
+    console.error(error);
+  });
