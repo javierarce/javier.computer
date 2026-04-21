85ee4ab304fa3703946fe0f2e081f77b579ac592
Javier Arce <javierarce@users.noreply.github.com>
2026-04-21 14:38:57 +0200

    Avoid repeating colors for adjacent book spines (#66)
    Co-authored-by: javierarce <4933+javierarce@users.noreply.github.com>

--- _plugins/random.rb
+++ _plugins/random.rb
@@ -3,6 +3,15 @@ module Jekyll
     def random_number(input)
       rand(input.to_i)
     end
+
+    def random_number_excluding(input, exclude)
+      max = input.to_i
+      return rand(max) if exclude.nil? || exclude.to_s.empty? || max <= 1
+      exclude_i = exclude.to_i
+      return rand(max) if exclude_i < 0 || exclude_i >= max
+      n = rand(max - 1)
+      n >= exclude_i ? n + 1 : n
+    end
   end
 end
 
